5ef060c336f37c7a0e679b321bb7c5fbbcd1f715
Branson Brooke <brooke.branson@yahoo.com>
2024-03-08 22:05:55 -0800

    update style

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function addTwoNums(a, b){
     try{
-        console.log(a + b)
+        if(typeof(a) != 'number'){
+            throw new ReferenceError('The first argument is not a number')
+        } else if (typeof(b) != 'number'){
+            throw new ReferenceError("The second argument is not a number")
+        } else {
+            console.log(a + b)
+        }
     }
     catch(err){
-        console.log(err)
+        console.log("Error!", err)
     }
 }
 
