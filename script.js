86515267db3438c0132d3f10a509383574d1a742
Branson Brooke <brooke.branson@yahoo.com>
2024-04-26 20:47:01 -0600

    Add functional form to grocery list
    Add functional event listener in script.js that
    will add item and quantity to desired list category

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-function addTwoNums(a, b){
-    try{
-        if(typeof(a) != 'number'){
-            throw new ReferenceError('The first argument is not a number')
-        } else if (typeof(b) != 'number'){
-            throw new ReferenceError("The second argument is not a number")
-        } else {
-            console.log(a + b)
-        }
-    }
-    catch(err){
-        console.log("Error!", err)
-    }
-}
+// document.querySelector("#item").addEventListener("keypress", addItem)
+// document.querySelector("#quantity").addEventListener("keypress", addItem)
+document.querySelector("#category").addEventListener("change", addItem)
 
-addTwoNums(5, "5")
+
+function addItem(e){
+    
+    /*
+    Listens for the final click on the category box, then adds the items and quantity
+    to the desired list category.
+    */
+    let item = document.querySelector("#item").value
+    let quantity = document.querySelector("#quantity").value
+    let category = document.querySelector("#category").value
+
+    let addToList = null
+
+    if (item == ""){
+        alert("Item missing, please add an item name")
+        addToList = false
+    }
+
+    if (quantity == ""){
+        alert("Missing quantity, add an amount.")
+        addToList = false
+    }
+
+    if (item && quantity){
+        document.querySelector(`#${category}`).innerHTML += `<li class='list-group-item'>${item} ${quantity}</li>`
+    }
+    console.log(item, quantity, category)
+
+    return item, quantity, category
+}
